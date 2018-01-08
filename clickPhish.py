c8f94a4c1ab44a015e5384c7c397d81b552fd752
Kilthw <kilthw@gmail.com>
2018-01-07 22:52:26 -0500

    Add files via upload

--- clickPhish.py
+++ clickPhish.py
@@ -5,52 +5,56 @@ pyautogui.FAILSAFE = True
 from time import sleep
 
 def setupOutlook():
+	print('Begin setup...')
 	#waiting for Back/Next button
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('Back_Grey.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/Back_Grey.png')
 	pyautogui.click(pyautogui.center(image)[0]+75,pyautogui.center(image)[1])
 	#waiting for Account Configuration
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('AccountConfig.png')
-	image = pyautogui.locateOnScreen('Back.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/AccountConfig.png')
+	image = pyautogui.locateOnScreen('C:/clickPhish/Back.png')
 	pyautogui.click(pyautogui.center(image)[0]+75,pyautogui.center(image)[1])
 	#waiting for Add New Account
 	sleep(3)
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('AddNewAccount.png')
-	image = pyautogui.locateOnScreen('Back.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/AddNewAccount.png')
+	image = pyautogui.locateOnScreen('C:/clickPhish/Back.png')
 	pyautogui.click(pyautogui.center(image)[0]+75,pyautogui.center(image)[1])
 	#waiting for Security Alert
 	sleep(3)
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('SecurityAlert.png')
-	image = pyautogui.locateOnScreen('No.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/SecurityAlert.png')
+	image = pyautogui.locateOnScreen('C:/clickPhish/No.png')
 	pyautogui.click(pyautogui.center(image)[0]-80,pyautogui.center(image)[1])
 	#waiting for Check Green Light
 	sleep(3)
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('checkGreen.png')
-	image = pyautogui.locateOnScreen('Back_Grey_Underline.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/checkGreen.png')
+	image = pyautogui.locateOnScreen('C:/clickPhish/Back_Grey_Underline.png')
 	pyautogui.click(pyautogui.center(image)[0]+75,pyautogui.center(image)[1])
 	#waiting for Security Alert
 	sleep(3)
 	image = None
 	while image == None:
-		image = pyautogui.locateOnScreen('SecurityAlert_Light.png')
-	image = pyautogui.locateOnScreen('No.png')
+		image = pyautogui.locateOnScreen('C:/clickPhish/SecurityAlert_Light.png')
+	image = pyautogui.locateOnScreen('C:/clickPhish/No.png')
 	pyautogui.click(pyautogui.center(image)[0]-80,pyautogui.center(image)[1])
 	
-
+print('Sleeping for 15 to wait for Outlook')
+sleep(15)
 #Initial Outlook Setup wait
+print('Looking for Outlook setup page...')
 setupCheck = None
 while setupCheck == None:
-	setupCheck = pyautogui.locateOnScreen('outlookSetup.png')
+	setupCheck = pyautogui.locateOnScreen('C:/clickPhish/outlookSetup.png')
 
-#sleep(10)
+print('Outlook setup located. Waiting for autopopulation to complete...')
+sleep(5)
 pyautogui.click(pyautogui.center(setupCheck))
 setupOutlook()
